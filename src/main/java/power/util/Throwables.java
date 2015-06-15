f3b3b68c34b6426e79188ab819fb33bb9d285bcc
miere <miere00@gmail.com>
2015-06-15 10:35:51 -0300

    Closes #4

--- src/main/java/power/util/Throwables.java
+++ src/main/java/power/util/Throwables.java
@@ -3,48 +3,69 @@ package power.util;
 import java.io.IOException;
 
 public class Throwables {
-	
+
 	private Throwables(){}
 
-	public static RuntimeException runtime( Throwable cause ) {
+	public static RuntimeException runtime( final Throwable cause ) {
 		return new RuntimeException(cause);
 	}
 
-	public static IllegalStateException illegalState( Throwable cause ) {
+	public static IllegalStateException illegalState( final Throwable cause ) {
 		return new IllegalStateException(cause);
 	}
 
-	public static IOException io( String message, Object...params ) {
+	public static IOException io( final String message, final Object...params ) {
 		return new IOException( String.format( message, params ) );
 	}
 
-	public static <V> V silently( CallableThatThrowsException<V> callable ) {
+	public static SilentlyException silently( final Throwable cause ) {
+		return new SilentlyException(cause);
+	}
+
+	public static SilentlyException silently( final Throwable cause, final String message, final Object...params  ) {
+		return new SilentlyException( String.format( message, params ), cause);
+	}
+
+	public static <V> V silently( final CallableThatThrowsException<V> callable ) {
 		try {
 			return callable.call();
-		// UNCHECKED: Really need to catch all exceptions here
-		} catch ( Throwable cause ) {
-		// CHECKED
-			throw runtime(cause);
+			// UNCHECKED: Really need to catch all exceptions here
+		} catch ( final Throwable cause ) {
+			// CHECKED
+			throw silently(cause);
 		}
 	}
 
-	public static void silently( RunnableThatThrowsException runnable ){
+	public static void silently( final RunnableThatThrowsException runnable ){
 		try {
 			runnable.run();
-		// UNCHECKED: Really need to catch all exceptions here
-		} catch ( Throwable cause ) {
-		// CHECKED
-			throw runtime(cause);
+			// UNCHECKED: Really need to catch all exceptions here
+		} catch ( final Throwable cause ) {
+			// CHECKED
+			throw silently(cause);
 		}
 	}
 
-	public static void runAnyWay( RunnableThatThrowsException runnable ){
+	public static void runAnyWay( final RunnableThatThrowsException runnable ){
 		try {
 			runnable.run();
-		// UNCHECKED: Really need to catch all exceptions here
-		} catch ( Throwable cause ) {
-		// CHECKED
+			// UNCHECKED: Really need to catch all exceptions here
+		} catch ( final Throwable cause ) {
+			// CHECKED
 			cause.printStackTrace();
 		}
+	}
+}
+
+class SilentlyException extends RuntimeException {
+
+	private static final long serialVersionUID = -4055198982175011545L;
+
+	public SilentlyException( final Throwable cause ) {
+		super( cause.getMessage(), cause, true, false );
+	}
+
+	public SilentlyException( final String message, final Throwable cause ) {
+		super( message, cause, true, false );
 	}
 }
